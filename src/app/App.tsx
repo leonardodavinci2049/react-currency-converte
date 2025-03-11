b67c46f8021c6918b475647478c90898192c3ddc
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-03-11 14:16:51 -0300

    start project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-bold text-red-500 underline uppercase">
+        New Project react
+      </h1>
+    </div>
+  );
+}
+
+export default App;
